1cae04b636926389ae492ea6ce6415167ce5f7f5
j0nomar <jonathan@Jonathans-MacBook-Pro.local>
2017-09-25 23:41:43 -0700

    files reorganization

---
+++ builder/README.md
@@ -0,0 +1,17 @@
+# Personal Site j0nomar.co 
+Create with [Pug2Boilerplate](https://github.com/jmarcelsw/Pug2Boilerplate)
+
+### Global Dependencies
+Make sure that you have Gulp and Browser Sync installed globally.
+* [Gulp](https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md)
+* [BrowserSync](https://www.browsersync.io/)
+
+### Project Dependencies
+```
+npm install
+```
+### Run
+Use gulp to compile into `app` directory and serve it.
+```
+gulp
+```
--- builder/gulpfile.js
+++ builder/gulpfile.js
@@ -5,22 +5,22 @@ var pug         = require('gulp-pug2')
 
 gulp.task('serve', ['sass','pug2'], function() {
     browserSync.init({
-        server: "./app"
+        server: "../"
     });
     gulp.watch("src/scss/*.scss", ['sass']);
     gulp.watch("src/*.pug", ['pug2']);
     gulp.watch("src/views/*.pug", ['pug2']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+    gulp.watch("../*.html").on('change', browserSync.reload);
 });
 gulp.task('sass', function() {
     return gulp.src("src/scss/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("app/css"))
+        .pipe(gulp.dest("../css"))
         .pipe(browserSync.stream());
 });
 gulp.task('pug2', function() {
   return gulp.src('src/*.pug')
   .pipe(pug({ yourTemplate: 'Locals' }))
-  .pipe(gulp.dest('app'))
+  .pipe(gulp.dest('../'))
 })
 gulp.task('default', ['serve']);
